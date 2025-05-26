04ea5f546de0ecc76ceef52f0a9581224e7acf47
HochangJin <hochang0115@gmail.com>
2025-05-26 15:30:51 +0900

    [ADD] Polygon Study

---
+++ Assets/02.Scripts/Study/StudyUnityEvent.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public class StudyUnityEvent : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("OnDisable");
+    }
+
+    void Update()
+    {
+        
+    }
+}
