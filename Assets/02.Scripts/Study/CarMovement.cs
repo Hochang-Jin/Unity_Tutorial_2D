846c2d51b2e4a0054bf45be0f3e2987c408e77dd
HochangJin <hochang0115@gmail.com>
2025-05-28 11:45:22 +0900

    [ADD] Study Collider and Rigidbody

---
+++ Assets/02.Scripts/Study/CarMovement.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class CarMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D carRB;
+    private float h;
+    
+    
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        
+        // transform 이동
+        // transform.position += Vector3.right * (h * moveSpeed * Time.deltaTime);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // Rigidbody 이동
+        carRB.linearVelocityX = h * moveSpeed;
+    }
+}
