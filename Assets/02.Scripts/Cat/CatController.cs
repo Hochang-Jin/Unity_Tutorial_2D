8b17932f168ace50b15fa15023d0b5c6156e6af0
HochangJin <hochang0115@gmail.com>
2025-06-04 10:43:30 +0900

    [ADD] Jump Animation

--- Assets/02.Scripts/Cat/CatController.cs
+++ Assets/02.Scripts/Cat/CatController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class CatController : MonoBehaviour
 {
     private Rigidbody2D catRB;
+    private Animator catAnim;
     // 땅 확인
     // private bool isGround = true;
     
@@ -19,6 +20,7 @@ public class CatController : MonoBehaviour
     void Start()
     {
         catRB = GetComponent<Rigidbody2D>();
+        catAnim = GetComponent<Animator>();
     }
 
     void Update()
@@ -26,6 +28,7 @@ public class CatController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumpCount)
         {
             catRB.AddForceY(jumpForce, ForceMode2D.Impulse);
+            catAnim.SetTrigger("Jump");
             jumpCount++;
         }
        
@@ -42,12 +45,13 @@ public class CatController : MonoBehaviour
         {
             // isGround = true;
             jumpCount = 0;
+            catAnim.SetTrigger("Ground");
         }
     }
 
     // private void OnCollisionExit2D(Collision2D other)
     // {
     //     if (other.gameObject.CompareTag("Ground"))
-    //         isGround = false;
+    //         catAnim.SetBool("isGround", false);
     // }
 }
