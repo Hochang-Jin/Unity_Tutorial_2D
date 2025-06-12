15f70ca96a5d16bbdb1a2c54692b78155bafdf3e
HochangJin <hochang0115@gmail.com>
2025-06-12 16:27:58 +0900

    [ADD] UI

--- Assets/02.Scripts/Study/Lotto/LottoGenerator.cs
+++ Assets/02.Scripts/Study/Lotto/LottoGenerator.cs
@@ -1,25 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class LottoGenerator : MonoBehaviour
 {
     // public int[] intArray = new int[10]; // 배열은 미리 만들어 놓는 방식
     public List<int> intList = new List<int>(); // 필요할 때마다 추가 / 삭제 / 삽입 가능한 방식
 
-    public int shakeCount = 1000;
+    public int shakeCount = 10000;
 
-    public string result;
+    public Button button;
+    public TextMeshProUGUI[] texts;
+    
     void Awake()
     {
         for (int i = 1; i < 46; i++) // i = 1 ~ 45까지의 반복
         {
             intList.Add(i);
         }
-        
     }
-    
-    void Start()
+
+    private void Start()
+    {
+        button.onClick.AddListener(OnBtn);
+    }
+
+    public void OnBtn()
     {
         for (int i = 0; i < shakeCount; i++)
         {
@@ -30,7 +40,15 @@ public class LottoGenerator : MonoBehaviour
             intList[ranInt1] = intList[ranInt2];
             intList[ranInt2] = temp;
         }
+        List<int> resultList = new List<int>();
+        for (int i = 0; i < 6; i++)
+            resultList.Add(intList[i]);
+        resultList.Sort();
+
+        for (int i = 0; i < 6; i++)
+        {
+            texts[i].text = resultList[i].ToString();
+        }
         
-        result = $"{intList[0]},{intList[1]},{intList[2]},{intList[3]},{intList[4]},{intList[5]}";
     }
 }
